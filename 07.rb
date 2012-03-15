67b3ce63a0b7126e8494882e61d71f0af02346f4
rickymclaren <rickymclaren@gmail.com>
2012-03-15 21:18:42 +0000

    Cleanup 07

--- 07.rb
+++ 07.rb
@@ -9,28 +9,3 @@ p = Prime.new
 10001.times { prime = p.succ() }
 puts prime
 
-# Use seive
-
-LIMIT = 6
-primes = Array.new(LIMIT+1, true) 
-
-(2..(Math.sqrt LIMIT)).each do |x|
-   if primes[x]
-      y = x * 2
-      while y <= LIMIT
-         primes[y] = false
-         y += x
-      end
-   end
-end
-
-count = 0
-(1..LIMIT).each do |x| 
-   if primes[x]
-      count += 1
-      puts primes[x] if count == 6
-   end
-end
-
-
-
