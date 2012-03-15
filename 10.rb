d0f2afc6e02f2e2583677e2a88f421315caf5a4e
rickymclaren <rickymclaren@gmail.com>
2012-03-15 22:07:55 +0000

    Explain slow Primes in 1.8

--- 10.rb
+++ 10.rb
@@ -3,6 +3,9 @@
 
 # Find the sum of all the primes below two million.
 
+# The Prime generator in ruby 1.8 is too slow over about 100000 primes
+# Had to write a Seive of Erasthones to go faster
+
 limit=2000000
 seive = Array.new(limit+1, true)
 
